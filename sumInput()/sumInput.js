605eadb7c1e3b1a9080bb005e8764017059168fb
lgolikova <milagoul@gmail.com>
2022-05-21 15:05:23 +0300

    в процессе

---
+++ sumInput()/sumInput.js
@@ -0,0 +1,17 @@
+// let button = document.querySelector('button');
+// button.addEventListener("click", sumInput());
+
+function sumInput() {
+
+    let numbers = [];
+    while (true) {
+    let value = prompt("Введите число", 0);
+    
+   
+}
+alert( sumInput() );
+
+
+
+
+}
---
+++ Задачи/script.js
@@ -0,0 +1,18 @@
+let arr = ['js', 'css', 'html'];
+console.log (arr[0]);
+
+let arr2 = [0, 1, false, 2, undefined, '', 3, null];
+let filteredArr2 = arr2.filter(el=>Boolean(el)!==false);
+console.log(filteredArr2); 
+
+let arr3 = [[1,2], [1,2,3], [1,2,3,4]];
+let el = arr3.findIndex(el=>el.length>3);
+console.log(el);
+
+//Или:
+let arr4 = [[1,2], [1,2,3], [1,2,3,4]];
+for (let i of arr4) {
+    if (i.length>3) {
+        console.log(arr4.indexOf(i));
+    }
+}
